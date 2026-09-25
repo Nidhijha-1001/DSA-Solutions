0573e2b21d3c31d429e2a1dca99e18c32d60e1f1
Nidhijha-1001 <nidhi.arcade2025@gmail.com>
2026-09-25 15:35:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.15%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int helper(int i , int j, vector<vector<int>>& dp,int m,int n){
+        if(i >= m or j >= n){
+            return 0;
+        }
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int right = helper(i,j+1,dp,m,n);
+        int down = helper(i+1,j,dp,m,n);
+
+        dp[i][j] = right+down;
+
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        int ans = helper(0,0,dp,m,n);
+
+        return ans;
+        
+    }
+};
